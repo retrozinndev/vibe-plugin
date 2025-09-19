4f8d3daea1f1a92576a101855ca5be75f93755bc
retrozinndev <joaovodias@gmail.com>
2025-09-19 18:22:11 -0300

    :wrench: chore: update to latest libvibe, use classes instead of instances

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,33 +1,39 @@
 import Gio from "gi://Gio?version=2.0";
-import { Plugin } from "../lib";
+
+import { Plugin } from "libvibe";
+import { Section } from "libvibe/src/vibe";
 
 
-const plugin = new Plugin({
-    name: "Vibe Plugin",
-    version: VIBE_PLUGIN_VERSION,
-    description: "A nice plugin for the Vibe Music Player!",
-    url: "https://github.com/retrozinndev/vibe-plugin",
-    implementsSections: true
-});
+export default class extends Plugin {
+    constructor() {
+        super({
+            name: "Vibe Plugin",
+            version: VIBE_PLUGIN_VERSION,
+            description: "A nice plugin for the Vibe Music Player!",
+            url: "https://github.com/retrozinndev/vibe-plugin",
+            implements: {
+                sections: true
+            }
+        });
+    }
 
-plugin.getSections = (_length?: number) => {
-    return [
-        {
-            title: "Section 1",
-            description: "This section is pretty nice!",
-            headerButtons: [{
-                label: "See developer's website",
-                onClicked: () => Gio.Subprocess.new([
-                    "xdg-open",
-                    "https://retrozinndev.github.io"
-                ], Gio.SubprocessFlags.NONE)
-            }]
-        },
-        {
-            title: "Section 2",
-            description: "Looking nice, section 2!!"
-        }
-    ];
-};
-
-export default { plugin };
+    getSections(_length?: number): Array<Section> | null {
+        return [
+            {
+                title: "Section 1",
+                description: "This section is pretty nice!",
+                headerButtons: [{
+                    label: "See developer's website",
+                    onClicked: () => Gio.Subprocess.new([
+                        "xdg-open",
+                        "https://retrozinndev.github.io"
+                    ], Gio.SubprocessFlags.NONE)
+                }]
+            },
+            {
+                title: "Section 2",
+                description: "Looking nice, section 2!!"
+            }
+        ];
+    }
+}
